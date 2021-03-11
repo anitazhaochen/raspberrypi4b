2ff7f5906ec7eded4c47fb5e7cabec64dd91e56c
zhaochen <zhaochen@>
2021-03-11 20:40:23 +0800

    添加README.md

--- ddns/ddns.sh
+++ ddns/ddns.sh
@@ -1,6 +1,6 @@
 docker run -d --restart=always --net=host \
-    -e "AKID=LTAI4Fvba1LRnTSLHr4mpzLK" \
-    -e "AKSCT=vJecAiuUbckK4CpoFgYLJBJ1BMZpEm" \
+    -e "AKID=LTAI4Fv1LnTSLHr4mpzLK" \
+    -e "AKSCT=vJecAiuU4CpoFgYLJBJ1BMZpEm" \
     -e "DOMAIN=yapi.enjoyms.com,www.enjoyms.com,enjoyms.com,nas.enjoyms.com,gitlab.enjoyms.com,aria.enjoyms.com" \
     -e "REDO=300" \
     -e "TTL=600" \
